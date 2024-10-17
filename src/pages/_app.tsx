1d0cc18b62abd251c95d1002ddab7d86fe0ad7ab
songyiiii <gksthddl43@daum.net>
2024-10-17 14:35:23 +0900

    Fix: commit test
    24.10.17

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,7 +22,7 @@ export default function App({ Component, pageProps }: AppProps) {
     // resize 이벤트 리스너 추가
     window.addEventListener('resize', handleResize);
 
-    // 컴포넌트 언마운트 시 이벤트 리스너 제거
+    // 컴포넌트 언마운트 시  이벤트 리스너 제거
     return () => {
       window.removeEventListener('resize', handleResize);
     };
