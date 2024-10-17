1fb98e2fae66077ae44afdca6968cbb84fa8b706
songyiiii <gksthddl43@daum.net>
2024-10-17 14:57:52 +0900

    Fix: notpc 삭제
    2024.10.17

--- src/features/NotPc/styled.ts
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const NotPcStyled = styled.div`
-  padding-top: 100px;
-`;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import NotPc from '@/features/NotPc';
 import Template from '@/layouts/Template';
 import { GlobalStyled } from '@/styles/global';
 import type { AppProps } from 'next/app';
@@ -6,40 +5,15 @@ import Head from 'next/head';
 import { useEffect, useState } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [notPc, setNotPc] = useState(false);
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth <= 1000) {
-        setNotPc(true);
-      } else {
-        setNotPc(false);
-      }
-    };
-
-    // 초기 width 확인
-    handleResize();
-
-    // resize 이벤트 리스너 추가
-    window.addEventListener('resize', handleResize);
-
-    // 컴포넌트 언마운트 시  이벤트 리스너 제거
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
   return (
     <>
       <Head>
         <title>🏠ChancePaceHost</title>
       </Head>
       <GlobalStyled />
-      {notPc ? (
-        <NotPc />
-      ) : (
         <Template>
           <Component {...pageProps} />
         </Template>
-      )}
     </>
   );
 }
