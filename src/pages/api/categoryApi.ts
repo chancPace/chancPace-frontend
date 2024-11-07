92ab11ef0c662ecb6993f19ff6080d519971ff06
songyiiii <gksthddl43@daum.net>
2024-11-07 16:44:24 +0900

    Fix: 개발환경에 따른 주소 변경
    2024.11.07

--- src/pages/api/categoryApi.ts
+++ src/pages/api/categoryApi.ts
@@ -1,12 +1,19 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:4000/api/category';
+const isLocal = process.env.NODE_ENV === 'development';
+
+const API_URL = `${
+  isLocal
+    ? `http://${process.env.NEXT_PUBLIC_LOCAL_HOST}:${process.env.NEXT_PUBLIC_LOCAL_PORT}`
+    : `http://${process.env.NEXT_PUBLIC_SERVER_HOST}:${process.env.NEXT_PUBLIC_SERVER_PORT}`
+}/api/category`;
+
 
 export const getCategories = async () => {
-    try {
-        const response = await axios.get(`${API_URL}/get-category`);
-        return response.data;
-    } catch (error) {
-        throw new Error('카테고리 불러오기 실패');
-    }
+  try {
+    const response = await axios.get(`${API_URL}/get-category`);
+    return response.data;
+  } catch (error) {
+    throw new Error('카테고리 불러오기 실패');
+  }
 };
--- src/pages/api/spaceApi.ts
+++ src/pages/api/spaceApi.ts
@@ -1,7 +1,13 @@
 import axios from 'axios';
 import Cookies from 'js-cookie'; // 쿠키 라이브러리 추가
 
-const API_URL = 'http://localhost:4000/api/space';
+const isLocal = process.env.NODE_ENV === 'development';
+
+const API_URL = `${
+  isLocal
+    ? `http://${process.env.NEXT_PUBLIC_LOCAL_HOST}:${process.env.NEXT_PUBLIC_LOCAL_PORT}`
+    : `http://${process.env.NEXT_PUBLIC_SERVER_HOST}:${process.env.NEXT_PUBLIC_SERVER_PORT}`
+}/api/space`;
 
 export const addNewSpace = async (spaceData: FormData) => {
   try {
@@ -12,7 +18,7 @@ export const addNewSpace = async (spaceData: FormData) => {
         Authorization: `Bearer ${token}`, // 가져온 토큰을 Authorization 헤더에 추가
       },
     });
-    return response.data;
+    return response.data || []; // undefined일 경우 빈 배열 반환
   } catch (error: any) {
     console.error('공간 등록 실패', error.response?.data || error.message);
     throw error;
@@ -46,12 +52,16 @@ export const getOneSpace = async (spaceId: string) => {
 export const updateSpace = async (spaceData: FormData, spaceId: string) => {
   try {
     const token = Cookies.get('token');
-    const response = await axios.patch(`${API_URL}/update-space?spaceId=${spaceId}`, spaceData, {
-      headers: {
-        // 'Content-Type': 'multipart/form-data',
-        Authorization: `Bearer ${token}`,
-      },
-    });
+    const response = await axios.patch(
+      `${API_URL}/update-space?spaceId=${spaceId}`,
+      spaceData,
+      {
+        headers: {
+          // 'Content-Type': 'multipart/form-data',
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
     console.log(response, '리스펀스 api');
     return response.data;
   } catch (error) {
